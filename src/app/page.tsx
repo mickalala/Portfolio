71811e8c4266d91c85a5c433d009354ede299454
mickalala <mickalarissa01@gmail.com>
2024-04-08 16:22:47 -0300

    feat: Basic infos configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 import { Header } from "@/components/Header/header";
 import "../styles/home.scss"
 import { Experience } from "@/components/Experience/experience";
+import { Informations } from "@/components/Informations/infos";
 
 export default function Home() {
   return (
     <main className="container" >
       <Header />
       <Experience />
-      <div>
-        <h2>Contato</h2>
-      </div>
-      <div>
-        <h2>Infos</h2>
-      </div>
+      <Informations />
       {/* área de links para projetos? */}
     </main>
   );
---
+++ src/components/Informations/infos.tsx
@@ -0,0 +1,16 @@
+import "./infos.scss"
+
+export function Informations() {
+    return (
+        <div className="infos" >
+            <div>
+                <h2>Languanges</h2>
+                <p>🇺🇸 English - Fluent</p>
+                <p>🇧🇷 Portuguese - Native Speaker</p>
+            </div>
+            <h2>Education</h2>
+            <p>🎓 UFPI- Física Bachelors Degree </p>
+        </div>
+    )
+
+}
