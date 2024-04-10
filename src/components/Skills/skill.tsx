b1179229feaa00b3347e65e2c77a9b4c2af5dbdb
mickalala <mickalarissa01@gmail.com>
2024-04-10 16:57:06 -0300

    feat: skills measures

---
+++ src/components/Skills/skill.tsx
@@ -0,0 +1,19 @@
+interface SkillProps {
+    img: string,
+    language: string,
+    years: number
+}
+import "./skill.scss"
+
+export function Skills({ language, img }: Readonly<SkillProps>) {
+    return (
+        <div className="skills">
+            <img src={img} alt="javaScript logo" />
+            <div className="experience-div">
+                <div className="experience-measure">
+                    {language}
+                </div>
+            </div>
+        </div>
+    )
+}
