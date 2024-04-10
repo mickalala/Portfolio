64a0339d39c3bb217967d7809cee48b0a8b131e6
mickalala <mickalarissa01@gmail.com>
2024-04-09 23:20:23 -0300

    fear: email button

--- src/components/Social-media-btns/social-media-bts.tsx
+++ src/components/Social-media-btns/social-media-bts.tsx
@@ -1,15 +1,21 @@
+import { EmailIcon } from "../Icons/email-icon";
 import { GitHubIcon } from "../Icons/github-icon";
 import { LinkedInIcon } from "../Icons/linkedIn-icon";
 import "./social-media-bts.scss"
 
 export function SocialMediaButtons() {
     return (
-        <div className="socialButtons">
-            <a href="https://github.com/mickalala" target="_blank">
-                <GitHubIcon />
-            </a>
-            <a href="https://www.linkedin.com/in/mickaelly-larissa-rodrigues/" target="_blank">
-                <LinkedInIcon />
+        <div className="buttons">
+            <div className="socialButtons">
+                <a href="https://github.com/mickalala" target="_blank">
+                    <GitHubIcon />
+                </a>
+                <a href="https://www.linkedin.com/in/mickaelly-larissa-rodrigues/" target="_blank">
+                    <LinkedInIcon />
+                </a>
+            </div>
+            <a className="btn-primary" href="mailto:mickalarissa01@gmail.com">
+                email <EmailIcon />
             </a>
         </div>
     )
