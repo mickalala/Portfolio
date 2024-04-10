af4ae3b2bd541fd53f5029a5c7261a2ef754d1e5
mickalala <mickalarissa01@gmail.com>
2024-04-10 16:57:38 -0300

    fix: details and buttons translatios

--- src/components/Experience/experience.tsx
+++ src/components/Experience/experience.tsx
@@ -1,15 +1,19 @@
 import { SectionTitle } from "../Section-title/section-title"
+import { Skills } from "../Skills/skill"
 import "./experience.scss"
 
 export function Experience() {
     return (
         <div className="experience">
-            <SectionTitle title="Experience"/>
+            <SectionTitle title="Experience" />
             <p>
                 One year working as a Freelancer Software Developer,
                 my principal languge is JavaScript, i do use frameworks as
                 React, Nest.
             </p>
+            <Skills language="javaScript" img="/js.png" years={1} />
+            <Skills language="React" img="/react.png" years={1} />
+            <Skills language="typeScript" img="/ts.png" years={1} />
         </div>
 
     )
